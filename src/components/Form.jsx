d9a47033df567c04c893daf09110c4e5570529bc
HeroldKiraly <kiraly-h@protonmail.com>
2023-01-16 08:48:57 -0500

    Added first Info tab

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,22 +6,60 @@ import Summary from './Summary';
 const Form = () => {
     return (
         <>
-            Step 1
-            Your info
-
-            Step 2
-            Select plan
-
-            Step 3
-            Add-ons
-
-            Step 4
-            Summary
-
-            <Info />
-            <Plan />
-            <Addons />
-            <Summary />
+            <main>
+                <aside className="sidebar">
+                    <div className="sidebar-item">
+                        <h1>1</h1>
+                        <div>
+                            <span>
+                                Step 1
+                            </span>
+                            <h3>
+                                Your info
+                            </h3>
+                        </div>
+                    </div>
+                    <div className="sidebar-item">
+                        <h1>2</h1>
+                        <div>
+                            <span>
+                                Step 2
+                            </span>
+                            <h3>
+                                Select plan
+                            </h3>
+                        </div>
+                    </div>
+                    <div className="sidebar-item">
+                        <h1>3</h1>
+                        <div>
+                            <span>
+                                Step 3
+                            </span>
+                            <h3>
+                                Add-ons
+                            </h3>
+                        </div>
+                    </div>
+                    <div className="sidebar-item">
+                        <h1>4</h1>
+                        <div>
+                            <span>
+                                Step 4
+                            </span>
+                            <h3>
+                                Summary
+                            </h3>
+                        </div>
+                    </div>
+                </aside>
+                <section>
+                    <Info />
+                    {/* <Plan /> */}
+                    {/* <Addons /> */}
+                    {/* <Summary /> */}
+                </section>
+            </main>
         </>
     );
 };
--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -3,20 +3,39 @@ import React from 'react';
 const Info = () => {
     return (
         <>
-            <h1>Info</h1>
-            Personal info
-            Please provide your name, email address, and phone number.
+            <div className="content">
+                <h1>Personal Info</h1>
+                <p>
+                    Please provide your name, email address, and phone number.
+                </p>
+                
+                <h2>
+                    Name
+                </h2>
+                <input></input>
+                e.g. Stephen King
 
-            Name
-            e.g. Stephen King
+                <h2>
+                    Email Address
+                </h2>
+                <input></input>
+                e.g. stephenking@lorem.com
 
-            Email Address
-            e.g. stephenking@lorem.com
+                <h2>
+                    Phone Number
+                </h2>
+                <input></input>
+                e.g. +1 234 567 890
+            </div>
 
-            Phone Number
-            e.g. +1 234 567 890
-
-            Next Step
+            <div className="buttons-container">
+                <button className="back-button">
+                    Go back
+                </button>
+                <button className="next-button">
+                    Next Step
+                </button>
+            </div>
         </>
     );
 };
