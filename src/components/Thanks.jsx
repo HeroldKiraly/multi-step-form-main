482a79a11d0070955d83f9fa36d308cd21824527
HeroldKiraly <kiraly-h@protonmail.com>
2023-01-20 13:14:31 -0500

    Imported Images

--- src/components/Thanks.jsx
+++ src/components/Thanks.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import thanksImage from '../assets/images/icon-thank-you.svg';
 
 const Thanks = () => {
     return(
         <>
             <div className="thanks-content">
-                <img src="./src/assets/images/icon-thank-you.svg"></img>
+                <img src={thanksImage}></img>
                 <h1 className="heading">Thank you!</h1>
                 <p className="subheading">
                     Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support, please feel free to email us at support@loremgaming.com.
